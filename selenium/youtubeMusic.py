95da4c423dcd4b50e76469f6c3a219a84a2a56d5
James Oh <jo9347@rit.edu>
2020-12-22 10:53:31 +0900

    Working except adding to the library. Have to click the right button

--- selenium/youtubeMusic.py
+++ selenium/youtubeMusic.py
@@ -3,11 +3,18 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 options = Options()
 # options.add_argument("--headless")
 # options.add_argument("--no-sandbox")
 options.add_argument("--incognito")
+
+def hover(self):
+    wd = webdriver_connection.connection
+    element = wd.find_element_by_link_text(self.locator)
+    hov = ActionChains(wd).move_to_element(element)
+    hov.perform()
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=481753852&memberKey=45064906&ref=copyurl&snsGate=Y")
@@ -53,11 +60,11 @@ driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
 # driver.find_element_by_xpath('<div class="BHzsHc">다른 계정 사용</div>')
 driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(username)
 driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
-time.sleep(1)
+time.sleep(3)
 driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input').send_keys(password)
 driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
-
-# driver.get("https://www.google.com/")
+time.sleep(5)
+# driver.get("https://www.youtube.com/")
 
 # email = driver.find_element_by_css_selector("#identifierId")
 # email.send_keys(username)
@@ -69,18 +76,25 @@ driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click(
 # time.sleep(2)
 
 # 2. Create the playlist
-# driver.get("https://music.youtube.com/")
-# driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/paper-icon-button[1]/iron-icon").click()
-# searchBar = driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/input")
-# for title in songs:
-#     searchBar.send_keys(title)
-#     searchBar.send_keys(Keys.RETURN)
-#     driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[1]/div[2]/ytmusic-responsive-list-item-renderer/ytmusic-menu-renderer/paper-icon-button/iron-icon")
-#     time.sleep(1)
-#     driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-popup-container/iron-dropdown/div/ytmusic-menu-popup-renderer/paper-listbox/ytmusic-menu-service-item-renderer[2]").click()
-# TODO TEST IT, Make it more structured!!!!
+driver.get("https://music.youtube.com/")
+driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/paper-icon-button[1]/iron-icon").click()
+searchBar = driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/input")
+for title in songs:
+    searchBar.clear()
+    searchBar.send_keys(title)
+    searchBar.send_keys(Keys.RETURN)
+    time.sleep(1)
+    hover = ActionChains(driver).move_to_element(driver.find_element_by_xpath('//*[@id="contents"]/ytmusic-responsive-list-item-renderer/div[2]'))
+    hover.perform()
+    time.sleep(1)
+    driver.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[1]/div[2]/ytmusic-responsive-list-item-renderer/ytmusic-menu-renderer/paper-icon-button/iron-icon').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="items"]/ytmusic-toggle-menu-service-item-renderer').click()
+    time.sleep(1)
+
+# TODO TEST IT, Make it more structured!!!! (Use Action Chain?)
 
 
-time.sleep(10)
+time.sleep(50)
 
 driver.quit()
