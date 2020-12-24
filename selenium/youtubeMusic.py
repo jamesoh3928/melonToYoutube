69d53d985b481113a8031c39b5d90fcf1a390fa3
James Oh <jo9347@rit.edu>
2020-12-24 21:04:26 +0900

    Adding to the playlist function works. Just continue if the song is not searched

--- selenium/youtubeMusic.py
+++ selenium/youtubeMusic.py
@@ -4,10 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
-# options.add_argument("--headless")
-# options.add_argument("--no-sandbox")
 options.add_argument("--incognito")
 
 def hover(self):
@@ -18,12 +19,22 @@ def hover(self):
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=481753852&memberKey=45064906&ref=copyurl&snsGate=Y")
-time.sleep(1)
 songs = []
 artists = []
 nextXpath = '/html/body/div/div[2]/div/div/div[2]/div/div[2]/div/span/a['
-numOfSongs = int(driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/div/h3/span").text[1:-1])
-numOfButtons = numOfSongs // 50 + 1
+numOfSongs = 0
+numOfButtons = 0
+delay = 10
+
+try:
+    numSongsElem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div/div[2]/div/div/div[2]/div/h3/span"))
+    )
+    numOfSongs = int(numSongsElem.text[1:-1])
+    numOfButtons = numOfSongs // 50 + 1
+except Exception as e:
+    print(e)
+
 for i in range(numOfButtons):
     try: 
         songObj = driver.find_elements_by_css_selector(".fc_gray")
@@ -37,11 +48,12 @@ for i in range(numOfButtons):
         time.sleep(1)
     except Exception as e:
         print(e)
+        continue
     
 #Checking if data were crawled
-print(songs)
-print(artists)
 print(len(songs), len(artists))
+for i in range(len(songs)):
+    print(songs[i], "     ", artists[i])
 
 #TODO 
 # 1. Login part to Youtube Music (do not actually login) - search python selenium secured login in youtube
@@ -53,49 +65,62 @@ username = user.readline()
 user.close()
 print("username: ", username)
 
+
 driver.get("https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent")
-time.sleep(1)
 
-driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
-# driver.find_element_by_xpath('<div class="BHzsHc">다른 계정 사용</div>')
-driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(username)
-driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
-time.sleep(3)
-driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input').send_keys(password)
-driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
-time.sleep(5)
-# driver.get("https://www.youtube.com/")
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="openid-buttons"]/button[1]'))
+    ).click()
+    WebDriverWait(driver, 100).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]'))
+    ).send_keys(username)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="identifierNext"]/div/button/div[2]'))
+    ).click()
+    time.sleep(2)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input'))
+    ).send_keys(password)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="passwordNext"]/div/button/div[2]'))
+    ).click()
 
-# email = driver.find_element_by_css_selector("#identifierId")
-# email.send_keys(username)
-# nextB = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
-# time.sleep(3)
-# passwordBar = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")
-# passwordBar.send_keys(password)
-# driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]").click()
-# time.sleep(2)
+except Exception as e:
+    print(e)
 
 # 2. Create the playlist
+time.sleep(3)
 driver.get("https://music.youtube.com/")
-driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/paper-icon-button[1]/iron-icon").click()
-searchBar = driver.find_element_by_xpath("/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/input")
-for title in songs:
+
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/paper-icon-button[1]/iron-icon'))
+).click()
+
+searchBar = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/input'))
+)
+
+# 3. Add to playlist
+for i in range(len(songs)):
     searchBar.clear()
-    searchBar.send_keys(title)
+    searchBar.send_keys(songs[i] + " " + artists[i] + " " + "노래")
     searchBar.send_keys(Keys.RETURN)
     time.sleep(1)
-    hover = ActionChains(driver).move_to_element(driver.find_element_by_xpath('//*[@id="contents"]/ytmusic-responsive-list-item-renderer/div[2]'))
-    hover.perform()
-    time.sleep(1)
-    driver.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[1]/div[2]/ytmusic-responsive-list-item-renderer/ytmusic-menu-renderer/paper-icon-button/iron-icon').click()
-    time.sleep(1)
-    driver.find_elements_by_xpath("//*[contains(text(), '재생목록에 추가')]")[0].click()
-    driver.find_elements_by_xpath("//*[contains(text(), 'melon')]")[0].click()
-    time.sleep(1)
+    try:
+        hover = ActionChains(driver).move_to_element(driver.find_element_by_xpath('//*[@id="contents"]/ytmusic-responsive-list-item-renderer/div[2]'))
+        hover.perform()
+        print("Hello")
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[1]/div[2]/ytmusic-responsive-list-item-renderer/ytmusic-menu-renderer/paper-icon-button/iron-icon'))).click()
+        time.sleep(1)
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="items"]/ytmusic-menu-navigation-item-renderer[2]'))).click()
+        time.sleep(1)
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="playlists"]/ytmusic-playlist-add-to-option-renderer/button'))).click()
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        continue
 
 # TODO TEST IT, Make it more structured!!!! (Use Action Chain?)
-
-
-time.sleep(50)
 
 driver.quit()
